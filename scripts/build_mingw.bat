73f8f7ff127facf14a4c4b9165731cb78851529c
Andrew Chen <llkiwi2006@gmail.com>
2015-04-25 15:11:30 +1200

    Fix errors in bat scripts
    Stupid Windows backslash

--- scripts/build_mingw.bat
+++ scripts/build_mingw.bat
@@ -3,7 +3,7 @@
 set SCRIPT_PATH=%~dp0
 
 pushd "%SCRIPT_PATH%\.."
-mkdir build
+if not exist build mkdir build
 cd build || goto :error
 cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug .. || goto :error
 mingw32-make -j4 || goto :error
--- scripts/eclipse_mingw.bat
+++ scripts/eclipse_mingw.bat
@@ -5,8 +5,10 @@ set SCRIPT_PATH=%~dp0
 pushd "%SCRIPT_PATH%\.."
 cmake -G"Eclipse CDT4 - MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_VERSION=4.4 . || goto :error
 if exist ..\.metadata (
-    md ..\.metadata\.plugins\org.eclipse.debug.core\.launches
-    copy "%SCRIPT_PATH%/client.launch" ../.metadata/.plugins/org.eclipse.debug.core/.launches/client.launch
+    if not exist ..\.metadata\.plugins\org.eclipse.debug.core\.launches (
+        md ..\.metadata\.plugins\org.eclipse.debug.core\.launches
+    )
+    copy "%SCRIPT_PATH%/client.launch" ..\.metadata\.plugins\org.eclipse.debug.core\.launches\client.launch
 )
 :error
 popd
