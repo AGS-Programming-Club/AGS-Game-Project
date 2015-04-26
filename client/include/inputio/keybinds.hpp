bb0a816bacbd303f1832b53b4d9bd64215b15754
James Brown <jamesphone180@gmail.com>
2015-04-26 18:04:48 +1200

    Added the rest of the documentation

--- client/include/inputio/keybinds.hpp
+++ client/include/inputio/keybinds.hpp
@@ -1,18 +1,32 @@
 #ifndef KEYBINDS
 #define KEYBINDS
 
+/** KEY_DOWN is called whenever a key is down on a poll call
+ * 	KEY_UP is called whenever a key is up on a poll call
+ * 	KEY_PRESSED is called whenever a key changes from up to down between this poll and the last
+ * 	KEY_RELEASED is called whenever a key changes from down to up between this poll and the last
+ */
 enum EventCode {KEY_DOWN, KEY_UP, KEY_PRESSED, KEY_RELEASED};
 
 namespace keybinds {
-	/** Adds an action to run on a key event */
+	/** Adds an action to run on a key event
+	 * @param run the action to run on keypress
+	 * @param keycode the GLFW keycode to run the action on
+	 * @param event the event type to run the action on @see EventCode
+	 **/
 	void add(void (*run)(), int keycode, EventCode event);
 
-	/** Removes an action to run on a key event */
+	/** Removes an action to run on a key event, NB you must use exactly the same paramaters as
+	 * @see keybinds::add or nothing will happen
+	 **/
 	void remove(void (*run)(), int keycode, EventCode event);
 
-	/** Polls the input devices and runs the added keybinds */
+	/** Polls the input devices and runs the added keybinds
+	 **/
 	void poll();
 
+	/** Adds a callback to make KEY_PRESSED and KEY_DOWN function
+	 **/
 	void init();
 }
 
--- client/include/log.hpp
+++ client/include/log.hpp
@@ -5,6 +5,12 @@
 
 enum Severity {INFO, WARN, ERROR};
 
+/** Logs a message
+ *
+ * 	@param level The importance of the message, info is just bacis information
+ * 	warn is something that needs to be addressed but need not crash the game
+ * 	error is something catastrofic that the game should not recover from.
+ **/
 void log(Severity level, std::string message);
 
 #endif
