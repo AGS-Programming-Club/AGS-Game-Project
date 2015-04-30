0fe3f3a217f6878bac27c51dbe39a4d0c1b71c83
Andrew Chen <llkiwi2006@gmail.com>
2015-04-30 17:30:17 +1200

    Fix stop copying eclipse launch configurations
    Eclipse seems to automatically pick up lanuch configurations,
    so there is no need for them to be copied into the default location

--- scripts/eclipse.sh
+++ scripts/eclipse.sh
@@ -9,9 +9,9 @@ cmake -G"Eclipse CDT4 - Unix Makefiles" \
       -DCMAKE_BUILD_TYPE=Debug \
       -DCMAKE_ECLIPSE_VERSION=4.4 \
       . &&
-if [ -d ../.metadata ]; then
-    mkdir -p ../.metadata/.plugins/org.eclipse.debug.core/.launches
-    cp -f "${SCRIPT_PATH}/client.launch" \
-          ../.metadata/.plugins/org.eclipse.debug.core/.launches/client.launch
-fi
+#if [ -d ../.metadata ]; then
+#    mkdir -p ../.metadata/.plugins/org.eclipse.debug.core/.launches
+#    cp -f "${SCRIPT_PATH}/client.launch" \
+#          ../.metadata/.plugins/org.eclipse.debug.core/.launches/client.launch
+#fi
 popd > /dev/null
--- scripts/eclipse_mingw.bat
+++ scripts/eclipse_mingw.bat
@@ -4,12 +4,12 @@ set SCRIPT_PATH=%~dp0
 
 pushd "%SCRIPT_PATH%\.."
 cmake -G"Eclipse CDT4 - MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_VERSION=4.4 . || goto :error
-if exist ..\.metadata (
-    if not exist ..\.metadata\.plugins\org.eclipse.debug.core\.launches (
-        md ..\.metadata\.plugins\org.eclipse.debug.core\.launches
-    )
-    copy "%SCRIPT_PATH%/client.launch" ..\.metadata\.plugins\org.eclipse.debug.core\.launches\client.launch
-)
+::if exist ..\.metadata (
+::    if not exist ..\.metadata\.plugins\org.eclipse.debug.core\.launches (
+::        md ..\.metadata\.plugins\org.eclipse.debug.core\.launches
+::    )
+::    copy "%SCRIPT_PATH%/client.launch" ..\.metadata\.plugins\org.eclipse.debug.core\.launches\client.launch
+::)
 :error
 popd
 
