43385602f61a0209a7347955c560b3f2c1854e9f
Andrew Chen <llkiwi2006@gmail.com>
2015-06-14 15:32:07 +1200

    Allow overriding cmake parameters in build scripts
    [ci skip]

--- scripts/eclipse.sh
+++ scripts/eclipse.sh
@@ -4,10 +4,14 @@ pushd `dirname $0` > /dev/null
 SCRIPT_PATH=`pwd -P`
 popd > /dev/null
 
+CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug}
+BUILD_UNIT_TESTS=${BUILD_UNIT_TESTS:-ON}
+CMAKE_ECLIPSE_VERSION=${CMAKE_ECLIPSE_VERSION:-4.4}
+
 pushd "${SCRIPT_PATH}/.." > /dev/null
 cmake -G"Eclipse CDT4 - Unix Makefiles" \
-      -DCMAKE_BUILD_TYPE=Debug \
-      -DBUILD_UNIT_TESTS=ON \
-      -DCMAKE_ECLIPSE_VERSION=4.4 \
+      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+      -DBUILD_UNIT_TESTS=${BUILD_UNIT_TESTS} \
+      -DCMAKE_ECLIPSE_VERSION=${CMAKE_ECLIPSE_VERSION} \
       .
 popd > /dev/null
